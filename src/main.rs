5b23318e169c0861a91151b79341c135824484f2
Ilya fanyShu <ilyashvy@gmail.com>
2022-01-25 22:09:31 +0300

    ctrl+q

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,19 @@
 use std::io::{self, Read};
 use termion::raw::IntoRawMode;
 
+const MASK: u8 = 0b0001_1111;
+
 fn main() {
     let _raw_terminal = io::stdout().into_raw_mode().unwrap();
     let stdin = io::stdin();
     let bytes_iter = stdin.bytes();
     for byte in bytes_iter {
         let byte = byte.unwrap();
-        let c = byte as char;
-        if c == 'q' {
+        println!("byte: {}\r", byte);
+        println!("char: {}\r", byte as char);
+        println!("binary: {:#b}\r", byte);
+        if byte == ('q' as u8 & MASK) {
             break;
         }
-        println!("{} {}\r", c, byte);
     }
 }
