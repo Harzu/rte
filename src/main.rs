5e06eec9a8d7b956f61bd21d9f3eb92b7c2e71a0
Ilya fanyShu <ilyashvy@gmail.com>
2022-01-25 14:17:02 +0300

    into_raw_mode

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
 use std::io::{self, Read};
+use termion::raw::IntoRawMode;
 
 fn main() {
+    let _raw_terminal = io::stdout().into_raw_mode().unwrap();
     let stdin = io::stdin();
     let bytes_iter = stdin.bytes();
     for byte in bytes_iter {
         let byte = byte.unwrap();
-        println!("{} {}", byte as char, byte);
+        let c = byte as char;
+        if c == 'q' {
+            break;
+        }
+        println!("{} {}\r", c, byte);
     }
 }
