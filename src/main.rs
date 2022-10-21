1e015d22095bec2a08469dd26a859e4ae4492ebf
Ilya fanyShu <ilyashvy@gmail.com>
2022-10-21 23:12:02 +0200

    clap

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,22 @@
 mod editor;
 
-use editor::Editor;
+use clap::{Command, Arg};
 
-fn main() -> Result<(), std::io::Error> {
-    let mut editor = Editor::new()?;
-    editor.run()
+const APP_NAME: &str = env!("CARGO_PKG_NAME");
+const VERSION: &str = env!("CARGO_PKG_VERSION");
+const FILE_PATH_ARG: &str = "filepath";
+
+fn main() {
+    let matches = Command::new(APP_NAME)
+        .version(VERSION)
+        .arg(
+            Arg::new(FILE_PATH_ARG)
+                .required(true)
+                .index(1)
+        )
+        .get_matches();
+    
+    let _file_path = matches.get_one::<String>(FILE_PATH_ARG).unwrap();
+    let mut editor = editor::Editor::new().unwrap();
+    editor.run().unwrap();
 }
