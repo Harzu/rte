d92c926bac3736a205a6d157ebb0ec83407520bf
Ilya fanyShu <ilyashvy@gmail.com>
2023-03-09 14:02:54 +0100

    handle stdin input in different thread

--- src/terminal.rs
+++ src/terminal.rs
@@ -1,4 +1,8 @@
 use std::io::{self, Write};
+use std::error;
+use std::time::Duration;
+use std::thread::{self, JoinHandle};
+use crossbeam::channel::{unbounded, select, Receiver, RecvError};
 use termion::{
     color,
     event::Key,
@@ -8,10 +12,13 @@ use termion::{
 };
 
 const PADDING_BUTTON: u16 = 2;
+const EXIT_CHARACTER: char = 'q';
+const SAVE_CHARACTER: char = 's';
 
 pub struct Terminal {
     stdout: AlternateScreen<RawTerminal<io::Stdout>>,
     size: ScreenSize,
+    input_event_handler: InputEventHandler,
 }
 
 pub struct ScreenSize {
@@ -19,15 +26,30 @@ pub struct ScreenSize {
     height: u16,
 }
 
+#[derive(Debug, Clone)]
 pub enum KeyEvent {
     Char(char),
-    Ctrl(char),
     Up,
     Down,
     Left,
     Right,
+    Exit,
+    SaveDocument,
     Backspace,
-    Unknown,
+    Unsupported,
+    Empty,
+}
+
+impl Drop for Terminal {
+    fn drop(&mut self) {
+        self.input_event_handler
+            .join_handle
+            .take()
+            .expect("join handler is not found")
+            .join()
+            .expect("join thread operation is failed")
+            .unwrap();
+    }
 }
 
 impl Terminal {
@@ -40,7 +62,8 @@ impl Terminal {
             size: ScreenSize {
                 width,
                 height: height.saturating_sub(PADDING_BUTTON),
-            }
+            },
+            input_event_handler: InputEventHandler::new(),
         })
     }
 
@@ -54,23 +77,6 @@ impl Terminal {
 
     pub fn height(&self) -> u16 {
         self.size.height
-    }
-
-    pub fn next_key() -> Result<KeyEvent, io::Error> {
-        if let Some(key) = io::stdin().keys().next() {
-            return match key? {
-                Key::Char(c) => Ok(KeyEvent::Char(c)),
-                Key::Ctrl(c) => Ok(KeyEvent::Ctrl(c)),
-                Key::Up => Ok(KeyEvent::Up),
-                Key::Down => Ok(KeyEvent::Down),
-                Key::Left => Ok(KeyEvent::Left),
-                Key::Right => Ok(KeyEvent::Right),
-                Key::Backspace => Ok(KeyEvent::Backspace),
-                _ => Ok(KeyEvent::Unknown),
-            }
-        }
-
-        Err(io::Error::new(io::ErrorKind::Other, "invalid input"))
     }
 
     pub fn cursor_hide() {
@@ -106,5 +112,66 @@ impl Terminal {
 
     pub fn reset_line_color() {
         print!("{}{}", color::Bg(color::Reset), color::Fg(color::Reset));
+    }
+
+    pub fn pull_key_event(&self) -> Result<KeyEvent, RecvError> {
+        select! {
+            recv(self.input_event_handler.input_event_receiver) -> event => {
+                event
+            }
+            default(Duration::from_secs(1)) => {
+                if let Some(input_event_handle) = &self.input_event_handler.join_handle {
+                    if input_event_handle.is_finished() {
+                        return Ok(KeyEvent::Exit);
+                    }
+                }
+
+                Ok(KeyEvent::Empty)
+            }
+        }
+    }
+}
+
+struct InputEventHandler {
+    join_handle: Option<JoinHandle<Result<(), Box<dyn error::Error + Send + Sync>>>>,
+    input_event_receiver: Receiver<KeyEvent>,
+}
+
+impl InputEventHandler {
+    fn new() -> Self {
+        let (input_event_sender, input_event_receiver) = unbounded::<KeyEvent>();
+        let join_handle = thread::spawn(move || -> Result<(), Box<dyn error::Error + Send + Sync>> {
+            loop {
+                let input_event = InputEventHandler::next_key()?;
+                input_event_sender.send(input_event.clone())?;
+                if let KeyEvent::Exit = input_event {
+                    break;
+                }
+            }
+            Ok(())
+        });
+
+        InputEventHandler {
+            join_handle: Some(join_handle),
+            input_event_receiver,
+        }
+    }
+
+    fn next_key() -> Result<KeyEvent, io::Error> {
+        if let Some(key) = io::stdin().keys().next() {
+            return match key? {
+                Key::Char(c) => Ok(KeyEvent::Char(c)),
+                Key::Up => Ok(KeyEvent::Up),
+                Key::Down => Ok(KeyEvent::Down),
+                Key::Left => Ok(KeyEvent::Left),
+                Key::Right => Ok(KeyEvent::Right),
+                Key::Backspace => Ok(KeyEvent::Backspace),
+                Key::Ctrl(EXIT_CHARACTER) => Ok(KeyEvent::Exit),
+                Key::Ctrl(SAVE_CHARACTER) => Ok(KeyEvent::SaveDocument),
+                _ => Ok(KeyEvent::Unsupported)
+            };
+        }
+
+        Ok(KeyEvent::Empty)
     }
 }
